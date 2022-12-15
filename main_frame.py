6ffc2ccb42185f945a6fdd54af2a0505b1aa6962
moyuu-az <sirius.nasa1000@gmail.com>
2022-12-15 06:46:06 +0000

    AP起動の様式確定

--- main_frame.py
+++ main_frame.py
@@ -10,9 +10,43 @@ import urequests
 import ujson
 import random
 
+# 自身のAPのSSIDを保持
+AP_SSID = ""
+# 自身のIPアドレス
+IP = ""
 
-def main():
-    pass
+# キャッシュデータ(テキストファイル)の削除処理
+def deleteCashFile():
+    fileList = os.listdir()
+    for fileNameOne in fileList:
+        if "recv" in fileNameOne:
+            os.remove(fileNameOne)
+
+# APを起動する際はこの関数を実行すること
+def activate_AP():
+    global wifi
+    global AP_SSID
+    global IP
+    
+    ap = network.WLAN(network.AP_IF)
+    AP_SSID = str(ap.config("essid"))
+    print("AP_SSID : ", AP_SSID)
+    portBind = 8080 # ソケット通信のポート
+    # 192.168.xxx.1のxxxをランダムで設定する
+    randomAddress = random.randrange(5,254)
+    IP = "192.168." + str(randomAddress) + ".1"
+    print(f"設定されるアクセスポイントIP : {IP}")
+
+def init_network():
+    activate_AP()
+    
+
+def main():    
+    # print(" --- キャッシュデータ削除処理 ---")
+    # deleteCashFile()
+    
+    # 初回のトポロジー設定
+    init_network()
 
 
 if __name__ ==  "__main__":
