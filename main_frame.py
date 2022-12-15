f4be74c73101904b6f71d20a1a53b06ad4453ca8
moyuu-az <sirius.nasa1000@gmail.com>
2022-12-15 06:14:37 +0000

    さあ始まりました！第四次メインフレームリファクタリング！今回はどのような末路(祀ろ)を辿るのでしょうか！

---
+++ main_frame.py
@@ -0,0 +1,19 @@
+import bmp280
+import utime
+import network
+import machine
+import _thread
+import socket
+import os
+from machine import Pin, I2C
+import urequests
+import ujson
+import random
+
+
+def main():
+    pass
+
+
+if __name__ ==  "__main__":
+    main()
